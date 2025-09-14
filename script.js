39cc8e84e642f5fd1dda80845f13d528796d0d0d
dengkun-blip <3114591686@qq.com>
2025-09-14 15:39:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ noButton.addEventListener("click", function () {
   if (clickCount === 1) mainImage.src = "images/1.mp4"
   if (clickCount === 2) mainImage.src = "images/2.mp4"
   if (clickCount === 3) mainImage.src = "images/3d.mp4"
-  if (clickCount === 4) mainImage.src = "images/4.mp4
+  if (clickCount === 4) mainImage.src = "images/4d.mp4"
   if (clickCount >= 5) mainImage.src = "images/5.mp4
 });
 
@@ -77,6 +77,7 @@ yesButton.addEventListener("click", function () {
   document.body.style.overflow = "hidden";
 
 });
+
 
 
 
