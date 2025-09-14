9bc23a0b6d3a955020a71ab7202677c4fe574693
dengkun-blip <3114591686@qq.com>
2025-09-14 11:55:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ noButton.addEventListener("click", function () {
 
   // 图片变化（前 5 次变化）
   if (clickCount === 1) mainImage.src = "images/1.mp4
-  if (clickCount === 2) mainImage.src = "images/think.png"; // 思考
+  if (clickCount === 2) mainImage.src = "images/2.mp4
   if (clickCount === 3) mainImage.src = "images/angry.png"; // 生气
   if (clickCount === 4) mainImage.src = "images/crying.png"; // 哭
   if (clickCount >= 5) mainImage.src = "images/crying.png"; // 之后一直是哭
@@ -77,3 +77,4 @@ yesButton.addEventListener("click", function () {
   document.body.style.overflow = "hidden";
 
 });
+
